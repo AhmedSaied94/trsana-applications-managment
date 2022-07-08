ac942a5a0a2db783ffc8d5b83cbe54bdbcd29b2d
AhmedSaied94 <ahmed.saeed311294@gmail.com>
2022-07-08 15:20:29 +0200

    add student resorce class

--- trsana/admin.py
+++ trsana/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Student, CommitteeEvaluation, StudentGrades
+from import_export import resources
 # Register your models here.
 
 
@@ -16,3 +17,9 @@ class CommitteeEvaluationAdmin(admin.ModelAdmin):
 @admin.register(StudentGrades)
 class StudentGradesAdmin(admin.ModelAdmin):
     pass
+
+
+class StudentResource(resources.ModelResource):
+    class Meta:
+        model = Student
+        fields = '__all__'
