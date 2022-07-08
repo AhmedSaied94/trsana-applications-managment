c6a78340fc5203d6e43b1b9607d087d9825628ba
AhmedSaied94 <ahmed.saeed311294@gmail.com>
2022-07-08 22:09:23 +0200

    customize student resource

--- trsana/admin.py
+++ trsana/admin.py
@@ -2,15 +2,32 @@ from django.contrib import admin
 from .models import Student, CommitteeEvaluation, StudentGrades
 from import_export import resources
 from import_export.admin import ImportExportMixin, ImportExportModelAdmin
+from import_export.fields import Field
 # Register your models here.
 
 
 class StudentResource(resources.ModelResource):
+    grades_total = Field(column_name='grades_total')
+    evaluation_total = Field(column_name='evaluation_total')
+
     class Meta:
         model = Student
-        exclude = ('student_pic', )
+
+        fields = ('id', 'name', 'file_no', 'group', 'batch', 'birthplace', 'address', 'birthdate', 'religion',
+                        'nationality', 'junior_cert_year', 'junior_cert_total', 'guardian', 'guardian_rel', 'guardian_phone', 'national_id',
+                        'grades_total', 'evaluation_total')
+
         export_order = ('id', 'name', 'file_no', 'group', 'batch', 'birthplace', 'address', 'birthdate', 'religion',
-                        'nationality', 'junior_cert_year', 'junior_cert_total', 'guardian', 'guardian_rel', 'guardian_phone', 'national_id')
+                        'nationality', 'junior_cert_year', 'junior_cert_total', 'guardian', 'guardian_rel', 'guardian_phone', 'national_id',
+                        'grades_total', 'evaluation_total')
+
+    def dehydrate_grades_total(self, obj):
+        total = str(round(obj.student_grades.total / 50 * 100)) + '%'
+        return str(obj.student_grades.total)
+
+    def dehydrate_evaluation_total(self, obj):
+        total = str(round(obj.committee_eval.total / 50 * 100)) + '%'
+        return str(obj.committee_eval.total)
 
 
 class CommitteeEvaluationResource(resources.ModelResource):
@@ -39,7 +56,7 @@ class StudentGradesAdmin(admin.ModelAdmin, ImportExportMixin):
 
 
 class StudentAdmin(ImportExportModelAdmin):
-    resource_class = Student
+    resource_class = StudentResource
 
 
 admin.site.register(Student, StudentAdmin)
